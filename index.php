e664457cdc0b0e39068fffd84d8265f8233fe418
kutsz <valik.kuts@gmail.com>
2017-05-12 17:05:04 +0300

    Last

--- index.php
+++ index.php
@@ -45,8 +45,33 @@ function data($str)
 
     return "$pieces[2] $pieces[1] $pieces[0]";
 }
+//**********************************************
+function getDrug($url)
+{
+  $file = file_get_contents($url);
+  $doc = phpQuery::newDocument($file);
 
+  foreach ($doc->find('#content .post a') as $drug) {
+      $drug = pq($drug);
 
+      $drug_name = $drug->find('h1')->text();
+
+      $url =$drug->attr("href");
+      $drug_reference = 'http:'.$url;
+
+      $data =$drug->find('header > div.meta')->text();
+      $last_revised = data($data);
+      $last_revised = date("Y-m-d", strtotime($last_revised));
+
+      addDrud($drug_name, $drug_reference, $last_revised);
+
+  }
+}
+//*****************F(p.242)*****************
+// $url = 'http://medlibrary.org/lib/rx/alpha_title/f/page/242/';
+// getDrug($url);
+
+//****************************************************
 
 function getInfoDrug($URL, $numOfpages)
 {
@@ -86,11 +111,15 @@ function getInfoDrug($URL, $numOfpages)
     }
 }
 
-//*************************************
+//**********************G (p.1-40)***************
 
+// $url_G = 'http://medlibrary.org/lib/rx/alpha_title/g/';
+// getInfoDrug($url_G, 40);
+
+//**************************************
 //$pages = [1=>450,229,520,386,195,242,161,174,145,12,64,343,402,249,189,448,33,156,254,333,34,153,16,21,3,98];
 // $pages = [4=>386,195];//D,E
-// $pages = [6=>242,161]; //F,G
+ //$pages = [6=>242,161]; //F,G
 //$pages = [8=>174,145]; //H,I
 // $pages = [10=>12,64]; //J,K
 // $pages = [12=>343,402]; //L,M
@@ -99,15 +128,16 @@ function getInfoDrug($URL, $numOfpages)
 // $pages = [18=>156,254]; //R,S
 // $pages = [20=>333,34]; //T,U
 // $pages = [22=>153,16]; //V,W
-$pages = [24=>21,3,98]; //X,Y,Z
+//$pages = [24=>21,3,98]; //X,Y,Z
 
 $url = 'http://medlibrary.org/lib/rx/alpha_title/';
 $html = file_get_contents($url);
 $doc = phpQuery::newDocument($html);
 
 
-$count = 24;
-$numA_Z = 26;
+
+$count = 0;
+$numA_Z = 0;
 while ($count<=$numA_Z) {
     $url = $doc->find("#content > article > ul > li:nth-child($count) > a")->attr("href");//url
 
